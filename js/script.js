ecf475757fcb32345e84076bffb2ee6638ef14c4
Joshua Davenport <87725934+jdavenport2021@users.noreply.github.com>
2023-04-16 21:43:30 -0500

    Adding a marker with a custom image

--- js/script.js
+++ js/script.js
@@ -3,10 +3,18 @@ let map;
 async function initMap() {
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
+  const myDorm = { lat: 41.8338255914747, lng: -87.62620677388293 };
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: 41.8338255914747, lng: -87.62620677388293 },
+    center: myDorm,
     zoom: 16,
+  });
+
+  new google.maps.Marker({
+    position: myDorm,
+    map,
+    title: "Where I currently reside.",
+    icon: "../media/home.png",
   });
 }
 
